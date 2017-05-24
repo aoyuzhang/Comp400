58e62369683fbfbfb23eb915fe9b63c731e21c0b
Haoyu Zhang <haoyu.zhang@mail.mcgill.ca>
2017-05-23 22:22:25 -0400

    Update GradientDecentLinearRegressionMultivariable.java
    Very bad convegent rate. might not converge??

--- GradientDecentLinearRegressionMultivariable.java
+++ GradientDecentLinearRegressionMultivariable.java
@@ -5,109 +5,197 @@ public class GradientDecentLinearRegressionMultivariable
 
 	public static void main(String[] args) 
 	{
-		/*Setting up the training examples*/
+		/*Setting up the training examples
+		 * By giving some random data points
+		 */
 		
 		final int nOfDataPoints=100;	// Number of training examples
-		Random rand = new Random(); 	// a random generator to generate random numbers
-		double[] x= new double[nOfDataPoints]; // x values of a training examples
-		double[] y= new double[100]; // the y values of training examples
-		for(int i=0;i<nOfDataPoints;i++)
+      	final int numberOfFeatures=3;
+		double[][] x= new double[nOfDataPoints][numberOfFeatures+1]; // x values of a training examples
+		double[][] y= new double[nOfDataPoints][1]; // the y values of training examples
+      	for(int i=0;i<nOfDataPoints;i++)
 		{
+          x[i][0]=1;
+          for(int j=1;j<numberOfFeatures+1;j++)
+			x[i][j] = Math.random()*10; // get random value for x ranging from 1 to 100
+          y[i][0]= aFunction(x[i]); // give y values according to x values and function aFunction defined below
 			
-			x[i] = Math.random(); // get random value for x ranging from 1 to 1000
-			y[i]= aFunction(x[i]); // give y values according to x values and function aFunction defined below
-			//System.out.println("x"+i+" is "+x[i]+" and y"+i+" is "+y[i]);
-		}
-		
-		
-		
-		
-		
-		
+		}  	
+	
 		/* We now apply gradient decent on the input examples.
 		 * We are now trying to minimize the cost function (1/(2*nOfDataPoints))*SumOverAllDataPOoints((a1x[i]+a0-y[i])^2)
 		 * a0=a0-alpha(partial(costFunction)/partial(a0))
 		 * a1=a1-alpha(partial(costFunction)/partial(a0))
 		 */
 		
-		final double alpha=1;// set the learning rate to be 0.5
-		double a0=6;
-		double a1=3; // pick initial a0 and a1
-		double temp0=6; 
-		double temp1=3; // used to store the the updated a0 and a1
-		int count=0; // use to count the number of iteration
-		do
+		final double alpha=0.00001;// set the learning rate to be 0.5
+		double[][] thetas=new double[numberOfFeatures+1][1];
+		double[][] temp= new double[numberOfFeatures+1][1];
+		int count=0;
+      	for(int i=0;i<thetas.length;i++)
+        	thetas[i][0]=Math.random();
+      	for(int i=0;i<thetas.length;i++)
+        	temp[i][0]=thetas[i][0];
+      	
+      	
+      	do
+      	{
+          	for(int i=0;i<thetas.length;i++)
+          		thetas[i][0]=temp[i][0];
+          	temp=updateThetas(x,y,alpha,thetas);
+      		count++;
+      		System.out.println("Iteration "+count+" The value of thetas is: ");
+      		printMatrix(thetas);
+      		System.out.println("And it is updated to: ");
+      		printMatrix(temp);
+      	}
+      	while(isCloseTo(0.0001,temp,thetas));
+        
+		
+	}
+	/* check if every value of a matrix is close to one and the other*/
+	public static boolean isCloseTo(double n,double[][]A, double [][]B )
+	{
+	  int aRows = A.length;
+      int aColumns = A[0].length;
+      int bRows = B.length;
+      int bColumns = B[0].length;
+      if (aColumns != bColumns||aRows != bRows) 
+      {
+        throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+      }
+	  for(int i=0;i<aRows;i++)
+	  {
+		  for(int j=0;j<aColumns;j++)
+		  {
+			  if(Math.abs(A[i][j]-B[i][j])>=n)
+				  return true;
+		  }
+	  }
+	  return false;
+	}
+	/* change a vector to a matrix*/
+	public static double[][] chageToMatrix(double[] a)
+	{
+		int length=a.length;
+		double[][] result=new double[1][length];
+		for(int i=0;i<length;i++)
+			result[0][i]=a[i];
+		return result;
+	}
+  	/* Function to update Thetas*/
+	public static double[][] updateThetas(double[][] x, double[][] y,double alpha, double[][] thetas)
+	{
+      double[][] result=new double[thetas.length][1];
+      for(int j=0;j<thetas.length;j++) // Go through each thetas one by one
+      {
+		double sum=0; // use to store the result
+		for(int i=0;i<x.length;i++) // iterate through each data points
 		{
-			a0=temp0;
-			a1=temp1;
-			temp0=updateA0(x,y,alpha,a0,a1);
-			temp1=updateA1(x,y,alpha,a0,a1);
-			count++;
-			System.out.println("Iteration "+count+" The value of a0 is "+a0+" And it is updated to "
-					+temp0+" The value of a1 is "+a1+" And it is updated to "+temp1);
-            //System.out.prinltn("Iteration "+count+" The value vost is: "+1/200) 
+			double[][] eval=mMultiplication(chageToMatrix(x[i]),thetas);
+			double evalR=eval[0][0];
+//			for(int m=0;m<x[i].length;m++) // get the vector product of x with theta
+//				eval=eval+x[i][m]+thetas[m][0];
+			sum=sum+(evalR-y[i][0])*x[i][j];
 		}
-		while(Math.abs(temp0-a0)>=0.00001||Math.abs(temp1-a1)>=0.00001);
-      
-		//System.out.println("The estimated value of a0 is "+a0+" And the estimated value of a1 is"+a1);
+		result[j][0]= result[j][0]-alpha*(sum/x.length);
+      }
+      return result;
+	}
+    
+	
+	/* A function that used to simulate the linear regression*/
+	public static double aFunction(double[] x) // a function to produce random data
+	{
+      	int length=x.length;
+      	double sum=0;
+      	for(int i=0;i<length;i++)
+        {
+          sum=sum+x[i]*(double)(i+1);
+        }
+        return sum;
+	}
+	
+	/* A function that add two matrices*/
+	public static double[][] addMatrices(double[][] A, double[][] B)
+	{
+		  int aRows = A.length;
+	      int aColumns = A[0].length;
+	      int bRows = B.length;
+	      int bColumns = B[0].length;
+	      if (aColumns != bColumns||aRows != bRows) 
+	      {
+	        throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+	      }
+	      double[][] C = new double[aRows][bColumns];
+	      for (int i = 0; i < aRows; i++) 
+	      {
+	        for (int j = 0; j < bColumns; j++) 
+	        {
+	          C[i][j] = 0.00000;
+	        }
+	      }
+	      for (int i = 0; i < aRows; i++) 
+	      { // aRow
+	          for (int j = 0; j < bColumns; j++) 
+	          { // bColumn
+	        	  C[i][j]=A[i][j]+B[i][j];
+	          }
+	      }
+	      return C;
+	 }
+	
+	/*A function to multiply two matrices*/
+    public static double[][] mMultiplication(double[][] A, double[][] B) 
+    {
 
-		
-		
-	}
-  	
-	public static double updateA0(double[] x, double[] y,double alpha, double a0, double a1)
-	{
-		double sum=0; // use to store the result
-		for(int i=0;i<x.length;i++)
-		{
-			sum=sum+a0+a1*x[i]-y[i];
-		}
-		return a0-alpha*(sum/x.length);
-	}
-	public static double updateA1(double[] x, double[] y,double alpha, double a0, double a1)
-	{
-		double sum=0; // use to store the result
-		for(int i=0;i<x.length;i++)
-		{
-			sum=sum+(a0+a1*x[i]-y[i])*x[i];
-		}
-		return a1-alpha*(sum/x.length);
-	}
-	public static double aFunction(double x) // a function to produce random data
-	{
-		return 9*+1;
-	}
+      int aRows = A.length;
+      int aColumns = A[0].length;
+      int bRows = B.length;
+      int bColumns = B[0].length;
+
+      if (aColumns != bRows) 
+      {
+        throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
+      }
+      double[][] C = new double[aRows][bColumns];
+      for (int i = 0; i < aRows; i++) 
+      {
+        for (int j = 0; j < bColumns; j++) 
+        {
+          C[i][j] = 0.00000;
+        }
+      }
+
+      for (int i = 0; i < aRows; i++) { // aRow
+        for (int j = 0; j < bColumns; j++) { // bColumn
+          for (int k = 0; k < aColumns; k++) { // aColumn
+            C[i][j] += A[i][k] * B[k][j];
+          }
+        }
+      }
+      return C;
+    }
+    
+    /*A function that print a matrix on the screen*/
+    public static void printMatrix(double[][] m)
+    {
+      try{
+        int rows = m.length;
+        int columns = m[0].length;
+        String str = "|\t";
+
+        for(int i=0;i<rows;i++){
+          for(int j=0;j<columns;j++){
+            str += m[i][j] + "\t";
+          }
+
+          System.out.println(str + "|");
+          str = "|\t";
+        }
+
+      }
+      catch(Exception e){System.out.println("Matrix is empty!!");}
+    }
 
 }
-public class MyMatrix {
-      Double[][] A;// = { { 4.00, 3.00 }, { 2.00, 1.00 } };
-      Double[][] B;// = { { -0.500, 1.500 }, { 1.000, -2.0000 } };
-
-      public static Double[][] multiplicar(Double[][] A, Double[][] B) {
-
-          int aRows = A.length;
-          int aColumns = A[0].length;
-          int bRows = B.length;
-          int bColumns = B[0].length;
-
-          if (aColumns != bRows) {
-              throw new IllegalArgumentException("A:Rows: " + aColumns + " did not match B:Columns " + bRows + ".");
-          }
-
-          Double[][] C = new Double[aRows][bColumns];
-          for (int i = 0; i < 2; i++) {
-              for (int j = 0; j < 2; j++) {
-                  C[i][j] = 0.00000;
-              }
-          }
-
-          for (int i = 0; i < aRows; i++) { // aRow
-              for (int j = 0; j < bColumns; j++) { // bColumn
-                  for (int k = 0; k < aColumns; k++) { // aColumn
-                      C[i][j] += A[i][k] * B[k][j];
-                  }
-              }
-          }
-
-          return C;
-      }
